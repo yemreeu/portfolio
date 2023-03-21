de65b731d7e964d77ff8248aa34b98299aca4d9c
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-03-21 18:31:41 +0300

    navbar has been modified and page styling has been developed

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./styles.scss"
 
 
 function Services() {
   return (
     
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem corrupti, laborum in tempore magnam esse, maxime est ullam aut voluptate debitis incidunt voluptatem animi possimus sint sequi odio totam illum et? Iusto eligendi saepe modi! Dolorum doloribus molestiae odio dolore ullam harum, ipsum cum sequi? Expedita ipsam iusto libero dolore.</div>
+    <div className="services">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem corrupti, laborum in tempore magnam esse, maxime est ullam aut voluptate debitis incidunt voluptatem animi possimus sint sequi odio totam illum et? Iusto eligendi saepe modi! Dolorum doloribus molestiae odio dolore ullam harum, ipsum cum sequi? Expedita ipsam iusto libero dolore.</div>
     
   );
 }
